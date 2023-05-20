df7e34049b32ec30586f1627df6aee531e527645
Uzytkownik <uzytkownik@kursGIT.pl>
2023-05-20 12:41:32 +0200

    add another type of relation

---
+++ get_author_by_article.py
@@ -0,0 +1,17 @@
+from session import session
+from models import Article, Author
+
+
+def main():
+    # as we did before
+    #   article = session.query(Article).get(1)
+    #   author = session.query(Author).get(article.author_id)
+    #   print(article, author)
+
+    # as we do now. we have to add relationship to class Article
+    article = session.query(Article).get(1)
+    print(article, article.author)
+
+
+if __name__ == '__main__':
+    main()
--- models.py
+++ models.py
@@ -41,6 +41,8 @@ class Article(Base):
         nullable=False
     )
 
+    author = relationship("Author")
+
     def __repr__(self):
         return f"Article title: {self.title}"
 
